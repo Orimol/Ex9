e0ed2b6580a23160c6a9acf975d6a02a399c9795
Orimol <33389705+Orimol@users.noreply.github.com>
2018-06-11 12:33:43 +0300

    Add files via upload

---
+++ TestCase.cpp
@@ -0,0 +1,23 @@
+//
+// Created by ori on 6/7/18.
+//
+#include "TestCase.hpp"
+
+
+TestCase::TestCase(string name, std::ostream& cerr):test_name(name),omessage(cerr) {
+    counter=1;
+    failer_count=0;
+}
+
+
+void TestCase:: print() {
+    ostringstream absorb;
+    absorb<<test_name<<": "<<failer_count<<" failed, "<<counter-failer_count-1<<" passed, "<<counter-1<< " total "<<endl<<"---"<<endl;
+    string s=absorb.str();
+    omessage.write(s.c_str(),s.size());
+    omessage.flush();
+    cout<<"\n";
+   // omessage.write(s.c_str(),s.size());
+
+
+}
---
+++ TestCase.hpp
@@ -0,0 +1,94 @@
+//
+// Created by ori on 6/7/18.
+//
+#include <iostream>
+#include <string>
+#include <sstream>
+
+
+
+#ifndef EX9_TESTCASE_HPP
+#define EX9_TESTCASE_HPP
+using namespace std;
+
+class TestCase {
+private:
+    string test_name;
+    ostream& omessage; // trying not use a declaration of ostream in the class
+    int counter;
+    int failer_count;
+
+public:
+    TestCase(string name, std::ostream& cerr);
+//
+//    template <typename T> TestCase check_equal(T A, T B);
+//
+//    template <typename T> TestCase check_different(T A, T B);
+//
+//    template <typename T> TestCase check_output(T A, string s);
+//
+//    template <typename T, typename T2, typename T3> TestCase check_function(T foo, T2 A, T3 B);
+
+
+    template <typename T> TestCase check_equal(T A, T B){
+        if (A==B) {}
+        else{
+            failer_count++;
+            ostringstream absorb;
+            absorb<<test_name<<": Failure in test #"<<counter<<A<<": should equal"<<B<<endl;
+            string s=absorb.str();
+            omessage.write(s.c_str(),s.size());
+        }
+        counter++;
+        return  *this;
+    }
+
+
+    template <typename T> TestCase check_different(T A, T B){
+        if (A==B) {
+            failer_count++;
+            ostringstream absorb;
+            absorb<<test_name<<": Failure in test #"<<counter<<A<<": is not different from "<<B<<endl;
+            string s=absorb.str();
+            omessage.write(s.c_str(),s.size());
+        }
+        counter++;
+        return  *this;
+    }
+
+    template <typename T> TestCase check_output(T A, string s){
+        ostringstream absorb;
+        absorb<<A;
+        if(absorb.str()!=s){
+            failer_count++;
+            absorb<<test_name<<": Failure in test # "<<counter<<": string value should be "<<s<<" but is "<<absorb.str()<<endl;
+            string s=absorb.str();
+            omessage.write(s.c_str(),s.size());
+        }
+        counter++;
+        return  *this;
+    }
+
+    template <typename T, typename T2, typename T3> TestCase check_function(T foo, T2 A, T3 B) {
+        T3 temp = foo(A);
+        if (temp == B) {}//true
+        else {
+            failer_count++;
+            ostringstream absorb;
+            absorb << test_name << ": Failure in test #" << counter << ": Function should return " << B << " but returned "
+                   << temp << "!" << endl;
+            string s=absorb.str();
+            omessage.write(s.c_str(),s.size());
+        }
+        counter++;
+        return *this;
+    }
+
+    void print();
+
+
+
+};
+
+
+#endif //EX9_TESTCASE_HPP
