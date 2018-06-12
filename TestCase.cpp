bf3611b1df34e86665a50115f15f461b1576290b
Orimol <33389705+Orimol@users.noreply.github.com>
2018-06-12 16:06:08 +0300

    Update TestCase.cpp

--- TestCase.cpp
+++ TestCase.cpp
@@ -12,7 +12,7 @@ TestCase::TestCase(string name, std::ostream& cerr):test_name(name),omessage(cer
 
 void TestCase:: print() {
     ostringstream absorb;
-    absorb<<test_name<<": "<<failer_count<<" failed, "<<counter-failer_count-1<<" passed, "<<counter-1<< " total "<<endl<<"---"<<endl;
+    absorb<<test_name<<": "<<failer_count<<" failed, "<<counter-failer_count-1<<" passed, "<<counter-1<< " total."<<endl<<"---"<<endl;
     string s=absorb.str();
     omessage.write(s.c_str(),s.size());
     omessage.flush();
